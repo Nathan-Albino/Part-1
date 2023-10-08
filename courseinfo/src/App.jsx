36c36d9ae47ce0c8d788a4ac84a095bc3577a48a
Nathan <nathan.albino01@gmail.com>
2023-10-07 21:15:47 -0400

    feature(App): add right/left click count and history

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -39,22 +39,63 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const App = () => {
-  const [counter, setCounter] = useState(0)
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>
+  }
+  return <div>button press history: {props.allClicks.join(' ')}</div>
+}
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    const updatedLeft = left + 1
+
+    setLeft(updatedLeft)
+    setAll(allClicks.concat('L'))
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    const updatedRight = right + 1
+
+    setRight(updatedRight)
+    setAll(allClicks.concat('R'))
+    setTotal(left + updatedRight)
+  }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="plus" />
-      <Button handleClick={decreaseByOne} text="minus" />
-      <Button handleClick={setToZero} text="zero" />
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <History allClicks={allClicks} />
+      <p>total: {total}</p>
     </div>
   )
 }
+
+// const App = () => {
+//   const [counter, setCounter] = useState(0)
+
+//   const increaseByOne = () => setCounter(counter + 1)
+//   const decreaseByOne = () => setCounter(counter - 1)
+//   const setToZero = () => setCounter(0)
+
+//   return (
+//     <div>
+//       <Display counter={counter} />
+//       <Button handleClick={increaseByOne} text="plus" />
+//       <Button handleClick={decreaseByOne} text="minus" />
+//       <Button handleClick={setToZero} text="zero" />
+//     </div>
+//   )
+// }
 
 // const App = () => {
 //   const course = {
